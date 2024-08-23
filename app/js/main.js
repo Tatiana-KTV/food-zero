8ac6e6cf005b571180d2c71c6bbccdea697ceeb2
Tatiana-KTV <tatiana.cosacenco@gmail.com>
2024-08-23 23:35:30 +0200

    added fonts and header section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
 $(function(){
 
 });
+
+// document.addEventListener('DOMContentLoaded', () => {
+
+//   //Mobile Menu
+//   const burger = document.querySelector('.burger'); //наша кнопка
+//   const mobileMenu = document.querySelector('.menu'); //мобильное меню
+//   const bodyLock = document.querySelector('body'); //ищем как селектор ТЕГА
+
+//   burger.addEventListener('click', () => {
+//     mobileMenu.classList.toggle('menu--active'); //когда меню открыто
+//     if (mobileMenu.classList.contains('menu--active')) { //Проверяем, есть ли у меню активный класс
+//       burger.classList.add('burger--active'); //Когда открыто, иконка становится крестиком
+//       bodyLock.classList.add('lock'); //Блокируем скролл при открытом меню
+//     } else { //Когда нету активного класса у меню
+//       burger.classList.remove('burger--active'); //Возвращает в исходное состояние
+//       bodyLock.classList.remove('lock'); //Разрешаем скроллить
+//     }
+//   });
+// });
